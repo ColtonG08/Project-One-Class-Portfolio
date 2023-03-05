33146ec35cd46b8625e74c46bf50f36348197ab5
ColtonG08 <grant.641@osu.edu>
2023-03-05 18:58:49 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function setup(){
+    // Happens only once
+    createCanvas(720, 400);
+}
+
+function draw(){
+    // Happens for every frame refresh of the browser
+    background(150);
+    // Draw a circle at location (30, 30) with a diameter of 20.
+circle(350, 150, 250);
+describe('white circle with black outline in mid of gray canvas');
+
+fill(255, 204, 0)
+
+triangle(30, 75, 58, 20, 86, 100);
+describe('white triangle with black outline in mid-right of canvas');
+
+triangle(30, 75, 58, 20, 86, 100);
+describe('white triangle with black outline in mid-right of canvas');
+}
